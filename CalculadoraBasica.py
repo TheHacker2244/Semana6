d5211050fd2ec32d5b1ffb89d4cc734789c6ff03
TheHacker2244 <Azzaz4199@gmail.com>
2025-03-04 09:44:53 -0600

    Guardar cambios

--- CalculadoraBasica.py
+++ CalculadoraBasica.py
@@ -1,1 +1,10 @@
-print("Hello Word")
+import random
+cadena1 = input("Ingrese la primera cadena:")
+cadena2 = input("Ingrese la segunda cadena:")
+if cadena1 == cadena2:
+    print("Las cadenas son iguiales.")
+else: print("Las cadenas son diferentes")
+if cadena1 > cadena2:
+    print (f'"{cadena1}""es mayor el orde alfabetico que cadena dos"{cadena2}".')
+else:
+     print (f'"{cadena2}""es mayor el orde alfabetico que cadena dos"{cadena1}".')
